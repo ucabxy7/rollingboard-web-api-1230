f730c8d33078c15238b22e885cef9346629ffc5c
Xiaoxia Yang <z5474541@ad.unsw.edu.au>
2026-03-31 18:43:56 +0800

    deloy database schema into aws rds

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import express from "express";
 
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware";
 import registerRoutes from "./routes";
+import fs from 'fs';
 
 dotenv.config();
 
@@ -15,8 +16,18 @@ app.use(
   }),
 );
 app.use(express.json());
+app.get('/', (req, res) => {
+  // 判断是否运行在 Docker 容器内（通用方法，不需要环境变量）
+  const isDocker = fs.existsSync('/.dockerenv') || fs.existsSync('/run/.containerenv');
 
-registerRoutes(app);
+  if (isDocker) {
+    res.send('我是【Docker 容器】运行的 → 无论是本地还是云端');
+  } else {
+    res.send('我是【本机 npm run dev】运行的 → 不是容器');
+  }
+});
+
+registerRoutes(app)
 
 app.use("/health-check", (_req, res) => {
   res.status(200).json({ message: "OK" });
--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "=== Running Prisma migrations ==="
+npx prisma migrate deploy
+
 echo "Starting production server"
 node dist/index.js
